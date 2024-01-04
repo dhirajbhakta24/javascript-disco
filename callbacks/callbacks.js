d8c1c82f18546a7b66eff8fd4ffda23e52150501
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2024-01-04 21:46:04 +0530

    Added all the Folders to Disco javaScript

---
+++ callbacks/callbacks.js
@@ -0,0 +1,25 @@
+//what is a callback function in js 
+
+
+//javascript is a synchronous and single threaded language 
+
+
+//blocking the main thread
+
+
+//power of callbacks??
+
+
+//deep about event listners
+
+
+//closures demo with event listeners
+
+
+//scope demo with event listners
+
+
+//garbage collection and remove event listners
+
+
+
